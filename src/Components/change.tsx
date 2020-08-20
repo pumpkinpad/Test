2419a56ca028f4520c8d7480fd88c34847267e14
Vahagn Lalayan <warhag@hotmail.de>
2020-08-20 15:17:12 +0200

    added new stuff

---
+++ src/Components/change.tsx
@@ -0,0 +1,10 @@
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+
+const change = memo(function change(props) {
+    return (
+        <div>
+            chasd
+        </div>
+    )
+export default change
